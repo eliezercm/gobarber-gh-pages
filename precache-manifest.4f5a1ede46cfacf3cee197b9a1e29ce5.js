a4bced9634b20786194362995332b17c31e04f66
Eliezer Martins <eliezermarttins@gmail.com>
2019-07-28 14:58:38 -0300

    Updates

---
+++ precache-manifest.4f5a1ede46cfacf3cee197b9a1e29ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583a3f0a0aa8ac4501437dbd52df9158",
+    "url": "/gobarber-gh-pages/index.html"
+  },
+  {
+    "revision": "dda0226a40decd2e8056",
+    "url": "/gobarber-gh-pages/static/css/2.35acde40.chunk.css"
+  },
+  {
+    "revision": "dda0226a40decd2e8056",
+    "url": "/gobarber-gh-pages/static/js/2.a86a1386.chunk.js"
+  },
+  {
+    "revision": "32762cb0ec1371d0a2aa",
+    "url": "/gobarber-gh-pages/static/js/main.439cdc5b.chunk.js"
+  },
+  {
+    "revision": "922e30d9952a5a6322c3",
+    "url": "/gobarber-gh-pages/static/js/runtime~main.f790f3ec.js"
+  },
+  {
+    "revision": "5228e44e341f369683fb79176300f786",
+    "url": "/gobarber-gh-pages/static/media/logo.5228e44e.svg"
+  },
+  {
+    "revision": "910146c364d0727cce21817cf33af6b4",
+    "url": "/gobarber-gh-pages/static/media/logo_purple.910146c3.svg"
+  }
+]);
